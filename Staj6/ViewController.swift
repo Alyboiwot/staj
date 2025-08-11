00acd040b1356026d3c4e3f9c9a664bfcbfc6942
Alyboiwot <uzunayr.ali@gmail.com>
2025-08-11 14:18:57 +0300

    Initial Commit

---
+++ Staj6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Staj6
+//
+//  Created by Ali Ünal UZUNÇAYIR on 11.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
