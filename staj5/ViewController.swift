b43780f13bb8b8774d97672a85aba1b86f3c3e3c
Ali Ünal <uzunayr.ali@gmail.com>
2025-09-13 14:56:21 +0300

    Add files via upload

---
+++ staj5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  staj5
+//
+//  Created by Ali Ünal UZUNÇAYIR on 8.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
